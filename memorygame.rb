ff976165de3e81bd2b697bd19d693c926d903ae6
Jorge Vega Ramirez <jorgevp5@gmail.com>
2016-05-19 17:53:20 -0400

    General outline in progress

--- memorygame.rb
+++ memorygame.rb
@@ -1,17 +1,40 @@
+require "pry"
 def game_over?
   false
 end
 
-=begin#Things that I will need:
-Board of cards
-recording match
-actual cards
+def grid
+  cards = [ 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8]
+  board = ("A".."P").to_a
+  grid = board.zip(cards).to_h
+  #binding.pry
+end
 
-=end
+def display_grid
+  print grid.keys
+end
+
+def choose_card
+  gets.chomp.upcase
+end
+
+def input_validation
+end
+
+def compare_cards grid, card1, card2
+  grid[card1] == grid[card2]
+
+end
+
+grid
+display_grid
+
 until game_over?
-  choose_card_one
-  choose_card_two
-  compare_cards
-  record_match
-  replay
+  first_card = choose_card
+  second_card = choose_card
+
+  p compare_cards grid, first_card, second_card
+  #binding.pry
+  #record_match
+  #replay
 end
